4629b19329d206fe1465e250b661d422ce2dfd97
muktesh-verticalfox <mdeshpande@verticalfox.com>
2019-09-04 14:47:38 +0530

    rspec chapter 1

---
+++ 01-getting-started/spec/sandwich_spec.rb
@@ -0,0 +1,12 @@
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sandwitch' do
+  let(:sandwich) { Sandwich.new('delicious',[]) }
+  it 'is delicious' do
+    expect(sandwich.taste).to eq 'delicious'
+  end
+  it 'lets me add toppings' do
+    sandwich.toppings << 'cheese'
+    expect(sandwich.toppings).not_to be_empty
+  end
+
+end
